a929f96c18734ba82ab2687cd7f4aadea2a7dc33
giuliovn <giuliovnturi@gmail.com>
2023-05-07 12:38:44 +0300

    deployment script

--- deployment.py
+++ deployment.py
@@ -1,27 +1,25 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+from pathlib import Path
+from shutil import copyfile
 
 
+def store_model_into_pickle(results_dir: Path, deploy_dir: Path):
+    files_to_deploy = [
+        "latestscore.txt",
+        "ingestedfiles.txt",
+        "encoder.pkl",
+        "trainedmodel.pkl",
+    ]
+    for file in files_to_deploy:
+        print(f"Copy {file} to {deploy_dir}")
+        copyfile(results_dir / file, deploy_dir)
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
 
+    results_dir = Path(config["output_folder_path"])
+    prod_deployment_dir = config["prod_deployment_path"]
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+    store_model_into_pickle(results_dir, prod_deployment_dir)
